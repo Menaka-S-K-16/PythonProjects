29344d9b5fc7d13358c351fce6516098762abf27
MenakaSK <menakask16011998@gmail.com>
2023-07-13 14:32:13 +0530

    ML file commit

--- ml.py
+++ ml.py
@@ -1,13 +1,15 @@
 import numpy
 import matplotlib.pyplot as plt
-#ds=numpy.random.uniform(1,9,10) #data distribution
+ds1=numpy.random.uniform(1,9,10) #data distribution
+
 #gaussian or normal data distribution ie) bell curve
 ds=numpy.random.normal(1,5,40000)
 print(ds)
+
 x=[5,6,7,8]
 y=[1,2,3,0]
-#to dip histogram
-#plt.hist(ds,100)
+#to display histogram
+plt.hist(ds,100)
 
 #scatter plot
 plt.scatter(x,y)
